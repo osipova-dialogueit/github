73bb8545ea99275fff3472e7119d375b96bf1646
Osipova Tatiana <osipova_dialogueit@mail.ru>
2020-07-22 13:16:43 +0300

    Dependency Injection 2
    Dependency Injection Homework List of Beans

--- ru.alishev.springcourse/src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ ru.alishev.springcourse/src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,38 +1,43 @@
 package ru.alishev.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Adm on 20.07.2020
  * @project ru.alishev.springcourse
  **/
 public class MusicPlayer {
-    private Music music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getVolume() {
         return volume;
     }
-
     public void setVolume(int volume) {
         this.volume = volume;
     }
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List music) {
+        this.musicList = music;
     }
     public MusicPlayer() { }
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusic(List music) {
+        this.musicList = music;
     }
     public void playMusic() {
-        System.out.println("Playing music " + music.getSong());
+        for (Music music: musicList) {
+            System.out.println("Playing music " + music.getSong());
+        }
     }
 }
---
+++ ru.alishev.springcourse/src/main/java/ru/alishev/springcourse/RapMusic.java
@@ -0,0 +1,13 @@
+package ru.alishev.springcourse;
+
+/**
+ * @author Adm on 21.07.2020
+ * @project ru.alishev.springcourse
+ **/
+public class RapMusic implements Music{
+
+    @Override
+    public String getSong() {
+        return "Drop it like it's hot";
+    }
+}
