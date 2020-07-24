e57af123e1df856d4c4ac3f282d5329f8a189578
Osipova Tatiana <osipova_dialogueit@mail.ru>
2020-07-24 21:18:24 +0300

    Аннотации @Scope, @Value, @PostConstruct, @PreDestroy.
    Аннотации @Scope, @Value, @PostConstruct, @PreDestroy.

--- ru.alishev.springcourse/src/main/java/ru/alishev/springcourse/TestSpring.java
+++ ru.alishev.springcourse/src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -11,8 +11,9 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
