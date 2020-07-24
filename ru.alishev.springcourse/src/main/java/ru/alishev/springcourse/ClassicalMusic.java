9967227d4c307f90fd9b4655de0d6ff7997092a5
Osipova Tatiana <osipova_dialogueit@mail.ru>
2020-07-24 19:54:48 +0300

    Bean Lifecycle
    Жизненный цикл бина (Bean Lifecycle). Init, Destroy и Factory методы.

--- ru.alishev.springcourse/src/main/java/ru/alishev/springcourse/ClassicalMusic.java
+++ ru.alishev.springcourse/src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -5,7 +5,18 @@ package ru.alishev.springcourse;
  * @project ru.alishev.springcourse
  **/
 public class ClassicalMusic implements Music{
+    private  ClassicalMusic() { } //приватный конструткор
 
+    public static ClassicalMusic getClassicalMusic() {  //фабричный метод
+        return new ClassicalMusic();
+    }
+    public void doMyInit() {
+        System.out.println("Initializing...");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Desrtucting...");
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
